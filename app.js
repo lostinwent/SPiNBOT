e5d415e3424d32ee24097726b52e7abe6b1df009
lostinwent <stein@steinw.com>
2013-06-20 17:45:10 -0400

    keyup/keydown all directions

--- app.js
+++ app.js
@@ -26,21 +26,81 @@ $(document).ready(function(){
 	// 	$.ajax(url, { dataType: 'jsonp', data: { strafe: 1 } } );
 	// });		
 
-	$(document).keydown(function(key){
-		if (key.keyCode == 87 ){
+	$(document).keydown(function(key){    //forward
+		if (key.keyCode === 87 ){
 			$.ajax(url, { dataType: 'jsonp', data: { forward: 1 } } );
 		}
 	});
 
 	$('body').keyup(function(key){
-		if(key.keyCode == 87){
+		if(key.keyCode === 87){
 			$.ajax(url, { dataType: 'jsonp', data: { forward: 0 } } );
+		}
+	});	
+
+	$(document).keydown(function(key){    //backward
+		if (key.keyCode === 83 ){
+			$.ajax(url, { dataType: 'jsonp', data: { forward: -1 } } );
 		}
 	});
 
-// 	$(document).keyup(function(key){
+	$('body').keyup(function(key){
+		if(key.keyCode === 83){
+			$.ajax(url, { dataType: 'jsonp', data: { forward: 0 } } );
+		}
+	});	
 
-// 	});
+	$(document).keydown(function(key){		//left
+		if (key.keyCode === 65 ){
+			$.ajax(url, { dataType: 'jsonp', data: { turn: -1 } } );
+		}
+	});
+
+	$('body').keyup(function(key){
+		if(key.keyCode === 65){
+			$.ajax(url, { dataType: 'jsonp', data: { turn: 0 } } );
+		}
+	});	
+
+
+	$(document).keydown(function(key){		//right
+		if (key.keyCode === 68 ){
+			$.ajax(url, { dataType: 'jsonp', data: { turn: 1 } } );
+		}
+	});
+
+	$('body').keyup(function(key){
+		if(key.keyCode === 68){
+			$.ajax(url, { dataType: 'jsonp', data: { turn: 0 } } );
+		}
+	});	
+
+
+	$(document).keydown(function(key){		//strafe left
+		if (key.keyCode === 81 ){
+			$.ajax(url, { dataType: 'jsonp', data: { strafe: -1 } } );
+		}
+	});
+
+	$('body').keyup(function(key){
+		if(key.keyCode === 81){
+			$.ajax(url, { dataType: 'jsonp', data: { strafe: 0 } } );
+		}
+	});	
+
+	$(document).keydown(function(key){		//strafe right
+		if (key.keyCode === 69 ){
+			$.ajax(url, { dataType: 'jsonp', data: { strafe: 1 } } );
+		}
+	});
+
+	$('body').keyup(function(key){
+		if(key.keyCode === 69){
+			$.ajax(url, { dataType: 'jsonp', data: { strafe: 0 } } );
+		}
+	});	
+
+
 
 });
 
