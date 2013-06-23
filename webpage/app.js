9daf24d805321863e3124675d4579f645fa5c1ed
lostinwent <stein@steinw.com>
2013-06-23 11:56:34 -0400

    adding jQuery effects to DOM elements, CSS progress

--- webpage/app.js
+++ webpage/app.js
@@ -1,12 +1,14 @@
+var url = "http://localhost:8071/motion-control/update";
+
 var colors = ["#f1bb62", "#ee5d85", "#caeaf1", "#eb5b68", "#f8f082",            "#c5c452",  "#82cfcd",  "#cddf7f",
                 "#f05622", "#c5de8e",
                 "#ee5d85", "#dedee9"];
 var directions = ["Forward", "Backward", "Turn Right", "Turn Left",
-                  "Strafe Right", "Strafe Left"];
+                     "Strafe Right", "Strafe Left"];
+
+var time = ["1", "2", "3"];
 
 var time = ["1", "2", "3", "1", "2", "3"];
-
-var url = "http://localhost:8071/motion-control/update";
 
 function randomSort() {
     return( parseInt( Math.random()*10 ) %2 );
@@ -31,43 +33,43 @@ console.log(possibilities)
   var startAngle = 0;
   var arc = Math.PI / 3;
   var spinTimeout = null;
-
+  
   var spinArcStart = 10;
   var spinTime = 0;
   var spinTimeTotal = 0;
-
+  
   var ctx;
-
+  
   function draw() {
     drawRouletteWheel();
   }
-
+  
   function drawRouletteWheel() {
     var canvas = document.getElementById("wheelcanvas");
     if (canvas.getContext) {
-      var outsideRadius = 200;
-      var textRadius = 160;
-      var insideRadius = 125;
-
+      var outsideRadius = 250;
+      var textRadius = 200;
+      var insideRadius = 150;
+      
       ctx = canvas.getContext("2d");
       ctx.clearRect(0,0,500,500);
-
-
+      
+      
       ctx.strokeStyle = "white";
       ctx.lineWidth = 0;
-
-      ctx.font = 'bold 12px sans-serif';
-
+      
+      ctx.font = 'bold 16px sans-serif';
+      
       for(var i = 0; i < 6; i++) {
         var angle = startAngle + i * arc;
         ctx.fillStyle = colors[i];
-
+        
         ctx.beginPath();
         ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
         ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
         ctx.stroke();
         ctx.fill();
-
+        
         ctx.save();
         ctx.fillStyle = "black";
         ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius, 250 + Math.sin(angle + arc / 2) * textRadius);
@@ -75,30 +77,30 @@ console.log(possibilities)
         var text = possibilities[i];
         ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
         ctx.restore();
-      }
-
+      } 
+      
       //Arrow
       ctx.fillStyle = "black";
       ctx.beginPath();
       ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
-      ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
+      ctx.lineTo(250 + 100, 250 - (outsideRadius + 5));
       ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
       ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
       ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
       ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
       ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
-      ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
+      ctx.lineTo(250 - 100, 250 - (outsideRadius + 5));
       ctx.fill();
     }
   }
-
+  
   $("#button").click(function(){
     spinAngleStart = Math.random() * 10 + 10;
     spinTime = 0;
     spinTimeTotal = Math.random() * 3 + 4 * 1000;
     rotateWheel();
   });
-
+  
   function rotateWheel() {
     spinTime += 30;
     if(spinTime >= spinTimeTotal) {
@@ -110,18 +112,17 @@ console.log(possibilities)
     drawRouletteWheel();
     spinTimeout = setTimeout('rotateWheel()', 30);
   }
-
+  
   function stopRotateWheel() {
     clearTimeout(spinTimeout);
     var degrees = startAngle * 180 / Math.PI + 90;
     var arcd = arc * 180 / Math.PI;
     var index = Math.floor((360 - degrees % 360) / arcd);
     ctx.save();
-    ctx.font = 'bold 30px sans-serif';
+    ctx.font = 'bold 40px Impact';
     var text = possibilities[index]
     ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
-    ctx.restore();
-
+    
     var result = text
     console.log(result);
 
@@ -141,7 +142,7 @@ console.log(possibilities)
     var tc = ts*t;
     return b+c*(tc + -3*ts + 3*t);
   }
-
+  
   draw();
 
 function whatToDo(c_direction, c_time) {
@@ -182,7 +183,7 @@ function whatToDo(c_direction, c_time) {
   }}
 }
 
-$(document).ready(function(){
+  $(document).ready(function(){
 
   $("#forward").click(function(){
     $.ajax(url, { dataType: 'jsonp', data: { forward: 1 } } );
@@ -206,7 +207,7 @@ $(document).ready(function(){
 
   $("#st-right").click(function(){
     $.ajax(url, { dataType: 'jsonp', data: { strafe: 1 } } );
-  });
+  });   
 
   $(document).keydown(function(key){    //forward
     if (key.keyCode === 87 ){
@@ -218,7 +219,7 @@ $(document).ready(function(){
     if(key.keyCode === 87){
       $.ajax(url, { dataType: 'jsonp', data: { forward: 0 } } );
     }
-  });
+  }); 
 
   $(document).keydown(function(key){    //backward
     if (key.keyCode === 83 ){
@@ -230,7 +231,7 @@ $(document).ready(function(){
     if(key.keyCode === 83){
       $.ajax(url, { dataType: 'jsonp', data: { forward: 0 } } );
     }
-  });
+  }); 
 
   $(document).keydown(function(key){    //left
     if (key.keyCode === 65 ){
@@ -242,7 +243,7 @@ $(document).ready(function(){
     if(key.keyCode === 65){
       $.ajax(url, { dataType: 'jsonp', data: { turn: 0 } } );
     }
-  });
+  }); 
 
 
   $(document).keydown(function(key){    //right
@@ -255,7 +256,7 @@ $(document).ready(function(){
     if(key.keyCode === 68){
       $.ajax(url, { dataType: 'jsonp', data: { turn: 0 } } );
     }
-  });
+  }); 
 
 
   $(document).keydown(function(key){    //strafe left
@@ -268,7 +269,7 @@ $(document).ready(function(){
     if(key.keyCode === 81){
       $.ajax(url, { dataType: 'jsonp', data: { strafe: 0 } } );
     }
-  });
+  }); 
 
   $(document).keydown(function(key){    //strafe right
     if (key.keyCode === 69 ){
@@ -276,16 +277,22 @@ $(document).ready(function(){
     }
   });
 
-  $('body').keyup(function(key){
+  $('body').keyup(function(key){  
     if(key.keyCode === 69){
       $.ajax(url, { dataType: 'jsonp', data: { strafe: 0 } } );
     }
-  });
+  }); 
 
   $(document).ready(function() {
      $('#sb3').click(function(e) {
           e.preventDefault();
     $('#myModal').reveal();
+     });
+  });
+
+  $(document).ready(function() {
+    $('#sb1').click(function(bbb){
+      $.scrollTo(".spin", 800, {easing:'swing'});
      });
   });
 });
